07dc17546504f5e844884e3d4eb452c2559a5e50
dgo8914 <dg8390@gmail.com>
2020-06-17 21:15:14 -0500

    refactory1

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,9 +1,3 @@
-<?php
-
-include("php/login_con.php");
-
-?>
-
 
 <!DOCTYPE html>
 <html lang="en">
--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('login');  
+
+/*---route to  home----*/ 
+Route ::view ('/main','main')->('main');    
 });
- 
+
+ /*---route to tracks--*/
+ route ::veiw ('/Tracks','Tracks')->('Tracks');
+});
