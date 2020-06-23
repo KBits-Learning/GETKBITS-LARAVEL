a241c79d91a3e1784c1c831c366167efda709c57
raulgonzalezjr <raulgonzalezjr@gmail.com>
2020-06-22 21:05:19 -0700

    added styles

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -14,31 +14,23 @@ use Illuminate\Http\Request;
 		return view('login');
 	}
 
-	public function checkCredentials(Request $request){
-{
-public  function show (){
-     return view('login');
+	//user credentials 
+	public function checkCredentials (Request $request) {
+		
+		$user = [];
+		$user['email'] = $request->get('email');
+		$user['password'] = $request->get('password');
+		$encrypted_password = sha1($user['password']);
 
-}
+		//user validation 
+		$users = DB::select('select id from users where email = ? AND  password = ?', [$user['email'], $encrypted_password]);
+		session (['user_id' => $users[0]->id]);
 
-//user credentials 
-public function checkCredentials (Request $request) {
-$user = [];
-$user['email'] = $request->get('email');
-$user['password'] = $request->get('password');
-$encrypted_password = sha1($user['password']);
+		//wrong user messege 
+		return back()->with('status','Somthing is wrong', 'Check your email and password');
 
-//user validation 
-$users = DB::select('select id from users where email = ? AND  password = ?', [$user['email'], $encrypted_password]);
-session (['user_id' => $users[0]->id]);
-
-//wrong user messege 
-return back()->with('status','Somthing is wrong', 'Check your email and password');
-
-
-//log in to home page 
-return redirect('homeController@index'); 
-
+		//log in to home page 
+		return redirect('homeController@index'); 
 	}
  }
 ?>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,38 +3,48 @@
     <head>
         <meta charset="utf-8"/>
         <title>KBITS</title>
+        <link href="{{ asset('css/all.css') }}" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/a81368914c.js"></script>
     <head/>    
     <body id="indexLogin">
-        <nav>
-            <a>Welcome to kbits<a/>
+        <nav class="navbar fixed-top navbar-light bg-light">
+          <a class="navbar-brand" href="/">Kbits</a>
         </nav>
-        <div class="form-sigin">
-            {!! form::open(['route'=>'loginForm']) !!}
-            <!--email login seccion-->
-             <div class = "form-group {{$error->has('email') ? 'has-error' : '' }}" >
-                 <div class="form-label-group">
-                    {!! Form::label('Email:') !!}
-                    {!! Form::text('email', old('email'), ['class'=>'form-control', 'placeholder'=>'Enter Email']) !!}
+        <div class="login-container">
+            <div class="login-img">
+                <img class src="images/kbits-login.svg">
+            </div>
+            <div class="form-sigin">
+                {!! Form::open(['route'=>'loginForm']) !!}
+                <!--email login seccion-->
+                 <div class = "form-group {{ $errors->has('email') ? 'has-error' : '' }}" >
+                    <div class="i">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="form-label-group">
+                        {!! Form::label('Email:') !!}
+                        {!! Form::text('email', old('email'), ['class'=>'form-control', 'placeholder'=>'Enter Email']) !!}
                     </div>
                     <span class="text-danger">{{ $errors->first('email') }}</span>                   
-             </div>
-             <!--password login seccion-->
-             <div class="form-group {{ $errors->has('password') ? 'has error' : '' }}">
-                  <div  class="form-label-group"> 
-                    {!! Form::label('password:') !!}   
-                    {!! Form::Text('pasword', old('pass'), ['class'=>'form-control', 'placeholder'=>'Enter Password']) !!}
-                  </div>
-                  <span class="text-danger">{{ $errors->first('password') }}</span>  
-             </div>
-             <!--login button-->
-             <div class="form-group">
-                 <button>LOGIN</button>
-             </div>
-          {!!Form::close()!!}   
-        </div>    
- <!-- wrong user messege-->    
+                 </div>
+                 <!--password login seccion-->
+                 <div class="form-group {{ $errors->has('password') ? 'has error' : '' }}">
+                      <div  class="form-label-group"> 
+                        {!! Form::label('password:') !!}   
+                        {!! Form::Text('pasword', old('pass'), ['class'=>'form-control', 'placeholder'=>'Enter Password']) !!}
+                      </div>
+                      <span class="text-danger">{{ $errors->first('password') }}</span>  
+                 </div>
+                 <!--login button-->
+                 <div class="form-group">
+                     <button>Login</button>
+                 </div>
+              {!!Form::close()!!}   
+            </div>    
+        <!-- wrong user messege-->    
         @if(session('status'))
-        {{ session('status')}}
+            {{ session('status') }}
         @endif
     </body>
     </head>    
+</html>
