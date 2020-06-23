e1249ddc8a662d7ea4b49f8a4e6418e74b81709a
ingglez89 <ingglez89@gmail.com>
2020-06-22 21:52:04 -0500

    change login

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -1,13 +1,27 @@
 <?php
-namespace app\Http\controllers;
 
-use App\Http\controllers\controllers;
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
 use App\User;
-use Illuminate\support\Facades\DB;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
+ class LoginController extends Controller
+ {
+	public function show(){
 
-class LoginController extends Controller
+		return view('login');
+	}
 
+	// get user credentials
+	// validate user credentials
+	// successfully pass user to their homepage
+	//		else return error
+
+	public function checkCredentials(Request $request){
+
+<<<<<<< HEAD
 {
 public  function show (){
      return view('login');
@@ -36,4 +50,19 @@ return redirect('homeController@index');
 
 
 
+=======
+		$user = [];
+		$user['email'] = $request->get('email');
+		$user['password'] = $request->get('password');
+
+		$encrypted_password = sha1($user['password']);
+
+    	$users = DB::select('select id from users where email = ? AND password = ?', [$user['email'], $encrypted_password]);
+
+    	session(['user_id' => $users[0]->id]);
+   	
+    	return redirect()->action('homeController@index');
+	}
+ }
+>>>>>>> 723bd7462fb43556cad040e5cbf596ce728794f0
 ?>
---
+++ app/Http/Controllers/tracksController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+ class TracksController extends Controller
+ {
+ 	 // public function show(){
+
+ 	 // }
+ }
+?>
