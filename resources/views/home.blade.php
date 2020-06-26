0bfd5ddea112a1e3b90d91ddf253b38ce6930f39
raulgonzalezjr <raulgonzalezjr@gmail.com>
2020-06-25 20:25:01 -0700

    fixed merging errors

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,8 @@
 <head>
     <meta charset="utf-8">
     <title>My Learnig Tracks</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
-
-    <link rel="stylesheet" href="style.css">
-
-    <link href="{{ asset('css/all.css') }}" rel="stylesheet">
-
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
